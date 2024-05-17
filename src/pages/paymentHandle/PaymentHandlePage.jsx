cee54bb7f452a7ecaacdc43e381faf9df6018be0
juanfe <lassojuanfe@gmail.com>
2024-05-17 16:43:53 -0500

    Ajusta manejador del pago para descargar PDF

--- src/pages/paymentHandle/PaymentHandlePage.jsx
+++ src/pages/paymentHandle/PaymentHandlePage.jsx
@@ -35,25 +35,38 @@ const PaymentHandlePage = () => {
     fetchPayment();
   }, []);
 
+  const imagesLoaded = (parentNode) => {
+    const imgElements = parentNode.querySelectorAll("img");
+    const promises = Array.from(imgElements).map((image) => {
+      if (image.complete) {
+        return Promise.resolve();
+      } else {
+        return new Promise((resolve) => {
+          image.onload = resolve;
+        });
+      }
+    });
+    return Promise.all(promises);
+  };
+
   const downloadPdf = () => {
     const element = document.getElementById("payment-details");
-
-    html2canvas(element, { scale: 2 })
-      .then((canvas) => {
-        const imgData = canvas.toDataURL("image/png");
-
-        const pdf = new jsPDF({
-          orientation: "portrait",
-          unit: "px",
-          format: [canvas.width, canvas.height],
+    imagesLoaded(element).then(() => {
+      html2canvas(element, { scale: 2 })
+        .then((canvas) => {
+          const imgData = canvas.toDataURL("image/png");
+          const pdf = new jsPDF({
+            orientation: "portrait",
+            unit: "px",
+            format: [canvas.width, canvas.height],
+          });
+          pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
+          pdf.save("payment-details.pdf");
+        })
+        .catch((err) => {
+          console.error("Error al generar el PDF: ", err);
         });
-
-        pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
-        pdf.save("payment-details.pdf");
-      })
-      .catch((err) => {
-        console.error("Error al generar el PDF: ", err);
-      });
+    });
   };
 
   if (loading)
